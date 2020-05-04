4f4a66e3babf942316c0f6d61196fba49b2b3897
Rozzini <Rozzinimr@gmail.com>
2020-05-04 15:28:24 +0300

    Basic registration added

---
+++ TESProject/TESProject/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TESProject.Models
+{
+    public class User : IdentityUser
+    {
+        public int UserId { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public string UserPassword { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
