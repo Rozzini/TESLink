177914d8019520de525eb01e4f8d5ae5173f0a8c
Rozzini <Rozzinimr@gmail.com>
2020-05-08 18:21:21 +0300

    General improvements for registration

--- TESProject/TESProject/Data/ApplicationDbContext.cs
+++ TESProject/TESProject/Data/ApplicationDbContext.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TESProject.Models;
 
 namespace TESProject.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<User> MyUsers { get; set; }
     }
 }
